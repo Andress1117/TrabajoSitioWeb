52c3283dc98dbe208b57a68940bd763331ddc0ab
Andress1117 <juanfelipe11172005@gmail.com>
2025-04-28 19:44:05 -0500

    Bien

--- hospital/src/main/java/com/example/hospital/repository/IMedicationRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.hospital.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-import com.example.hospital.model.Medication;
-
-public interface IMedicationRepository extends JpaRepository<Medication, Integer> {
-}
